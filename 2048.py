cee97d5349f4a3d58c5ffde0b0adcbd972ac82f1
Oriane Joeger <oriane.joeger@gmail.com>
2020-02-11 19:55:40 +0100

    Affichage de la grille

--- 2048.py
+++ 2048.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 from affichage import *
 from saisie import *
 from mouvements import *
--- affichage.py
+++ affichage.py
@@ -7,22 +7,35 @@ def bienvenue():
 	print("\td --> droite")
 	print("\t0 --> arreter la partie")
 
-def afficher_grille(grille, taille):
+def afficher_separateur(taille):
 	separator = '+'
-	for i in range(len(grille)):
+	for i in range(taille):
 		separator += str('-').rjust(taille + 2, '-')
 		separator += '+'
-
-	for ligne in grille:
-		display = ''
-		for cell in ligne:
-			display += '| '
-			if cell > 0:
-				display += str(cell).rjust(taille, ' ')
-			else:
-				display += str('').rjust(taille, ' ')
-			display += ' '
-
-		print(separator)
-		print(display + '|')
 	print(separator)
+
+def afficher_ligne_vide(taille):
+	affichage = ''
+	for i in range(taille):
+		affichage += '|'
+		affichage += str('').rjust(taille + 2, ' ')
+	print(affichage + '|')
+
+def afficher_ligne_rempli(taille, ligne):
+	display = ''
+	for cell in ligne:
+		display += '| '
+		if cell > 0:
+			display += str(cell).rjust(taille, ' ')
+		else:
+			display += str('').rjust(taille, ' ')
+		display += ' '
+	print(display + '|')
+
+def afficher_grille(grille, taille):
+	for ligne in grille:
+		afficher_separateur(taille)
+		afficher_ligne_vide(taille)
+		afficher_ligne_rempli(taille, ligne)
+		afficher_ligne_vide(taille)
+	afficher_separateur(taille)
